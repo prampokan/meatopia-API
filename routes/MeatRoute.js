15a811bd6259be4a48171e391bd3dbba1020a84f
prampokan <prampokan@gmail.com>
2024-06-06 00:21:25 +0700

    gitignore

--- routes/MeatRoute.js
+++ routes/MeatRoute.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { deleteMeat, getMeatById, getMeats, saveMeat, updateMeat } from "../controllers/MeatController.js";
+import { verifyUser } from "../middleware/AuthUser.js";
 
 const router = express.Router()
 
-router.get('/meats', getMeats)
-router.get('/meats/:id', getMeatById)
-router.post('/meats', saveMeat)
-router.patch('/meats/:id', updateMeat)
-router.delete('/meats/:id', deleteMeat)
+router.get('/meats', verifyUser, getMeats)
+router.get('/meats/:id', verifyUser, getMeatById)
+router.post('/meats', verifyUser, saveMeat)
+router.patch('/meats/:id', verifyUser, updateMeat)
+router.delete('/meats/:id', verifyUser, deleteMeat)
 
 export default router
